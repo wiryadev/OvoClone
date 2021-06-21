55b26603d58847132c106281f9a0f2acc8a38450
wiryadev <abrarwiryawan@gmail.com>
2021-06-21 08:24:23 +0700

    Change lazyColumn to standard column and finish setting category list

--- app/src/main/java/com/wiryadev/ovoclone/ui/home/ProfileScreen.kt
+++ app/src/main/java/com/wiryadev/ovoclone/ui/home/ProfileScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -108,14 +110,67 @@ private val accountCategories = listOf(
 fun AccountSection() {
     BaseSurface(
         title = "Akun",
-        titleTextStyle = MaterialTheme.typography.h5,
+        titleTextStyle = MaterialTheme.typography.h4,
         paddingValues = PaddingValues(top = SPACE_X2)
     ) {
-        LazyColumn {
-            itemsIndexed(accountCategories) { index, item ->
-                ProfileItemRow(item = item)
+        Column {
+            accountCategories.forEachIndexed { index, category ->
+                ProfileItemRow(item = category)
 
                 if (index != (accountCategories.size - 1)) {
+                    Divider(
+                        modifier = Modifier.padding(horizontal = SPACE_X2)
+                    )
+                }
+            }
+        }
+    }
+}
+
+val securityCategories = listOf(
+    Category("S1", "Ubah Security Code", R.drawable.ic_settings_security_code_new)
+)
+
+@Composable
+fun SecuritySection() {
+    BaseSurface(
+        title = "Keamanan",
+        titleTextStyle = MaterialTheme.typography.h4,
+        paddingValues = PaddingValues(top = SPACE_X2)
+    ) {
+        Column {
+            securityCategories.forEachIndexed { index, category ->
+                ProfileItemRow(item = category)
+
+                if (index != (securityCategories.size - 1)) {
+                    Divider(
+                        modifier = Modifier.padding(horizontal = SPACE_X2)
+                    )
+                }
+            }
+        }
+    }
+}
+
+val aboutCategories = listOf(
+    Category("T1", "Keuntungan Pakai OVO", R.drawable.ic_settings_ovo_benefit_new),
+    Category("T2", "Panduan OVO", R.drawable.ic_settings_panduan_ovo_new),
+    Category("T3", "Syarat dan Ketentuan", R.drawable.ic_settings_ovo_terms),
+    Category("T4", "Kebijakan Privasi", R.drawable.ic_settings_kebijakan_privasi_new),
+)
+
+@Composable
+fun AboutSection() {
+    BaseSurface(
+        title = "Keamanan",
+        titleTextStyle = MaterialTheme.typography.h4,
+        paddingValues = PaddingValues(top = SPACE_X2)
+    ) {
+        Column {
+            aboutCategories.forEachIndexed { index, category ->
+                ProfileItemRow(item = category)
+
+                if (index != (aboutCategories.size - 1)) {
                     Divider(
                         modifier = Modifier.padding(horizontal = SPACE_X2)
                     )
@@ -130,11 +185,14 @@ fun ProfileScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(PepperLighter),
+            .background(PepperLighter)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(SPACE_X1),
     ) {
         OvoIdSection()
         AccountSection()
+        SecuritySection()
+        AboutSection()
     }
 }
 
